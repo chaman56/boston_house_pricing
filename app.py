94d3061772b7284d83e48d350339c7c4f7aa5ba6
Chaman56 <ychaman56@gmail.com>
2024-01-03 00:49:20 +0530

    compled web application

--- app.py
+++ app.py
@@ -21,7 +21,16 @@ def predict_api():
     new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
     output = regmodel.predict(new_data)
     print(output[0])
-    return jsonify(output[0]) 
+    return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    print(data)
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text="Predicted House value is {}.".format(output))
      
 
 if __name__ == '__main__':
